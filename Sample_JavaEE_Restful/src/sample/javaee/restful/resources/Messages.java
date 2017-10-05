958a6a68d4c61fdc57d3c58bb500e2a63143c7ae
shengjun-zh <zhu_sj@126.com>
2017-10-05 23:41:17 +0800

    update

--- Sample_JavaEE_Restful/src/sample/javaee/restful/resources/Messages.java
+++ Sample_JavaEE_Restful/src/sample/javaee/restful/resources/Messages.java
@@ -1,6 +1,8 @@
 package sample.javaee.restful.resources;
 
+import java.util.List;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -17,10 +19,20 @@ public class Messages extends Application {
 	@Path("/addMessage")
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_JSON)
-	// http://localhost:8080/Sample_JavaEE_Restful/resources/RestServiceJson/getMessage
+	// http://localhost:8080/Sample_JavaEE_Restful/resources/Messages/addMessage
 	public Message addMessage(Message msg) {
 		MessagesDB.addMessages(msg);
 		return msg;
+
+	}
+        
+        @GET
+	@Path("/getMessages")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	// http://localhost:8080/Sample_JavaEE_Restful/resources/Messages/getMessages
+	public List<Message> getMessages() {
+		return MessagesDB.getMessages();
 
 	}
 }
